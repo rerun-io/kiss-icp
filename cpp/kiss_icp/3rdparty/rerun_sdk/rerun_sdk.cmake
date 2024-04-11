108ec6c610204de7a037c9df490412d7c3441de7
Clement Rey <cr.rey.clement@gmail.com>
2024-04-11 16:01:50 +0200

    Rerun 0.15 (#1)

--- cpp/kiss_icp/3rdparty/rerun_sdk/rerun_sdk.cmake
+++ cpp/kiss_icp/3rdparty/rerun_sdk/rerun_sdk.cmake
@@ -1,4 +1,4 @@
 
 include(FetchContent)
-FetchContent_Declare(rerun_sdk URL https://github.com/rerun-io/rerun/releases/download/prerelease/rerun_cpp_sdk.zip)
+FetchContent_Declare(rerun_sdk URL https://github.com/rerun-io/rerun/releases/download/0.15.0/rerun_cpp_sdk.zip)
 FetchContent_MakeAvailable(rerun_sdk)
